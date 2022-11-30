58ad4ad96f6b110ec737cb80cc62e7f2aefa4f89
Quentin Cavenel <53335284+kavneluxedy@users.noreply.github.com>
2022-11-30 15:44:42 +0100

    Create Animated Navbar Menu
    + Add new background image for homepage,
    + misc
    
    Co-Authored-By: Bparsis <benparsis@gmail.com>

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,19 +1,22 @@
 import React, { FC } from "react";
 
 interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
-  className?: string;
+	className?: string;
+	disabled?: boolean;
 }
 
-const Button: FC<ButtonProps> = ({ children, onClick, className }) => {
-  return (
-    <button
-      type="button"
-      className={(className) ? className : "button"}
-      onClick={onClick}
-    >
-      {children}
-    </button>
-  );
+const Button: FC<ButtonProps> = ({ children, onClick, className, disabled = false }) => {
+
+	return (
+		<button
+			type="button"
+			className={(className) ? className : "button"}
+			onClick={onClick}
+			disabled={disabled}
+		>
+			{children}
+		</button>
+	);
 };
 
 export default Button;
