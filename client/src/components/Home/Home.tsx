1f8eae1e0b9f65e374238a36572af2dbaf9d9409
Quentin Cavenel <53335284+kavneluxedy@users.noreply.github.com>
2022-12-09 18:22:13 +0100

    Create Navbar Menu for Homepage and others pages
    Co-Authored-By: Bparsis <benparsis@gmail.com>

---
+++ client/src/components/Home/Home.tsx
@@ -0,0 +1,59 @@
+import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { AppContext } from "../../utils/ContextProvider";
+import Button from "../Button";
+import MainRegister from "../auth/MainRegister";
+import MainLogin from "../auth/MainLogin";
+import useModal from "../../utils/hooks/useModal";
+import ChoiceGameType from "../game/ChoiceGameType";
+import HomepageMenu from "./HomepageMenu";
+
+const Home = () => {
+	const { handleVisibility, Modal } = useModal();
+
+	const AppCtx = useContext(AppContext);
+	if (AppCtx === null) {
+		return <></>;
+	}
+	const { user, isLobbyVisible } = { ...AppCtx };
+
+	return (
+		<>
+			<div className="background-wrapper">
+
+				<div className="home-wrapper">
+
+					<div className="hidden"></div>
+
+					<div className="absolute-height">
+						<Button
+							onClick={handleVisibility}>
+							JOUER
+						</Button>
+					</div>
+
+
+					{isLobbyVisible && (
+						<div id="auth">
+							{!user.isConnected && (
+								<>
+									<MainRegister />
+									<MainLogin />
+								</>
+							)}
+						</div>
+					)}
+
+					<HomepageMenu />
+
+					<Modal title={"JOUER"}>
+						<ChoiceGameType />
+					</Modal>
+
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Home;
---
+++ client/src/utils/Router.tsx
@@ -0,0 +1,34 @@
+import React, { useContext, useRef } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NotFound404 from "../pages/404";
+import Layout from "../pages/Layout";
+import Home from "../components/home/Home";
+import Admin from "../components/admin/Admin";
+import Embed from "../pages/Embed";
+import { AppContext } from "./ContextProvider";
+import PublicMatch from "../components/game/PublicMatch";
+import PrivateMatch from "../components/game/PrivateMatch";
+
+const Router = (): JSX.Element => {
+	const AppCtx = useContext(AppContext);
+	const { colorTheme } = { ...AppCtx! };
+
+	return (
+		<div className="App" data-theme={colorTheme}>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Layout />}>
+						<Route index element={<Home />} />
+						<Route path="admin" element={<Admin />} />
+						<Route path="game" element={<PublicMatch Modal={undefined} handleVisibility={undefined} />} />
+						<Route path="game" element={<PrivateMatch />} />
+						<Route path="embed" element={<Embed />} />
+						<Route path="*" element={<NotFound404 />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+};
+
+export default Router;
